ba314ebc4735b7f93a6eef554081254867c0eb58
cd3429 <125622988+cd3429@users.noreply.github.com>
2023-05-08 15:54:40 +0530

    Core FLutter

--- Core_Flutter/chapter2/2.3/FL_2_3_t1.dart
+++ Core_Flutter/chapter2/2.3/FL_2_3_t1.dart
@@ -1,3 +1,13 @@
+/*
+
+Write a Dart program to concate list element
+value with hello.
+For exmple,
+input: [1, 2, 3]
+output: [1 hello, 2 hello, 3 hello]
+
+*/
+
 import 'dart:io';
 
 void main() {
